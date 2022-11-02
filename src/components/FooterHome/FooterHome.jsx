697dc3965bd8aa06650a09dd58c21498d12f784b
danhNg <danh.nguyenthanh102@gmail.com>
2022-11-03 03:55:07 +0700

    fix-gitignore

---
+++ src/components/FooterHome/FooterHome.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FooterHome() {
+  return <div className="bg-dark text-center text-white py-3">FooterHome</div>;
+}
---
+++ src/pages/HookDemo/UseStateDemo/UseStateDemo.jsx
@@ -0,0 +1,8 @@
+//rfc
+import React from 'react'
+
+export default function UseStateDemo() {
+  return (
+    <div className='container my-2'>UseStateDemo</div>
+  )
+}
